9138ca81258510039a2952c87175bf3b39fb6bb2
eliott <eliott.chauviere@etudiant.univ-lr.fr>
2026-01-28 09:48:18 +0100

    Ajout de recuperation de la liste des questions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,59 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, use } from 'react';
 import './App.css';
 
 function App() {
-  const [question, setQuestion] = useState(null);
+  const [questions, setQuestions] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(null);
+  const [score, setScore] = useState(null);
+  const [showResults, setShowResults] = useState(false);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
-  const [isCorrect, setIsCorrect] = useState(null);
-  const [loading, setLoading] = useState(true);
 
-  // Fonction pour récupérer une question aléatoire
-  const fetchRandomQuestion = async () => {
-    setLoading(true);
-    setSelectedAnswer(null);
-    setIsCorrect(null);
+  // On charge toutes les questions
+  const fetchAllQuestions = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/questions/random');
+      const response = await fetch('http://localhost:5000/api/questions');
       const data = await response.json();
-      setQuestion(data);
+      setQuestions(data);
     } catch (error) {
       console.error("Erreur lors de la récupération :", error);
-    } finally {
-      setLoading(false);
     }
   };
 
   useEffect(() => {
-    fetchRandomQuestion();
+    fetchAllQuestions();
   }, []);
 
   const handleAnswer = (option) => {
     if (selectedAnswer) return; // Empêche de répondre plusieurs fois
     setSelectedAnswer(option);
-    setIsCorrect(option === question.correctAnswer);
+    
+    if (option === question[currentIndex].correctAnswer) {
+      setScore(score + 1);
+    }
   };
 
-  if (loading) return <div className="loader">Chargement du quiz...</div>;
-  if (!question) return <div>Aucune question trouvée. Lancez le script seed !</div>;
+  const nextQuestion = () => {
+    setSelectedAnswer(null);
+    if (currentIndex + 1 < questions.length) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      setShowResults(true); // On affiche la fin du quiz
+    }
+  };
+
+//  if (loading) return <div className="loader">Chargement du quiz...</div>;
+//  if (!question) return <div>Aucune question trouvée. Lancez le script seed !</div>;
+  if (questions.length === 0) return <div className="loader">Chargement du quiz...</div>;
+
+  if (showResults === true) {
+    return (
+      <div className='quiz-result'>
+        <h1>Votre score</h1>
+
+        <h2>{score}/{questions.length}</h2>
+      </div>
+    )
+  }
 
   return (
     <div className="quiz-container">
